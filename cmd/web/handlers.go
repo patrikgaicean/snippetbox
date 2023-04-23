8cc5cabd9ef6a99a6d596b5d81d4a004044d372c
unknown <patrikgaicean07@gmail.com>
2023-04-23 13:50:49 +0300

    first commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"example/snippetbox/pkg/models"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.render(w, r, "home.page.html", &templateData{Snippets: s})
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+
+	s, err := app.snippets.Get(id)
+	if err == models.ErrNoRecord {
+		app.notFound(w)
+		return
+	} else if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.render(w, r, "show.page.html", &templateData{Snippet: s})
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed)
+		return
+	}
+
+	title := "0 snail"
+	content := "0 snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n- Kobayashi"
+	expires := "7"
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module example/snippetbox
+
+go 1.11
+
+require github.com/go-sql-driver/mysql v1.4.0 // indirect
